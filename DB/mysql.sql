56ad64c092a863e9e525b95b8d74f01f81fed364
Ramjee Singh <ramjee.india@gmail.com>
2023-10-20 21:07:16 +0530

    sqld

---
+++ DB/mysql.sql
@@ -0,0 +1,14 @@
+mysql -u root -p
+ show databases;
+
+ create database ramjee
+
+ workbench
+
+ create table actor (f_name NVARCHAR(20), l_name NVARCHAR(30), Networth INTEGER)
+
+
+ CLEAR CONSOLE : \!cls  
+ for mac \!clear    
+
+
